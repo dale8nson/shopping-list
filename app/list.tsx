310a0fd0f9f543fa5b0084268844321c6439d9e9
dale8nson <daletristanhutchinson@gmail.com>
2024-01-08 15:40:52 +1100

    fixed it to clear re-render interval during editing

--- app/list.tsx
+++ app/list.tsx
@@ -1,11 +1,11 @@
 'use client';
 import type { ReactNode } from 'react';
-import { useState, useEffect, useRef, useMemo } from 'react';
+import { useState, useEffect, useRef, useMemo, useCallback } from 'react';
 import { OrderList } from 'primereact/orderlist';
 import { Checkbox } from 'primereact/checkbox';
 import { InputText } from 'primereact/inputtext';
 import { Inplace, InplaceDisplay, InplaceContent } from 'primereact/inplace';
-        
+
 import type { WithId, Document, InsertOneResult } from "mongodb";
 import { addItem } from '@/actions';
 import { useDebounce } from 'primereact/hooks';
@@ -21,39 +21,51 @@ const List = ({ baseUrl }: { baseUrl: string }) => {
   // const itemsRef = useRef(null);
   const [itemInputValue, setItemInputValue] = useState('');
   const [addItemResult, setAddItemResult] = useState<InsertOneResult<Document> | null>(null);
-  const [itemsNeedUpdate, setItemsNeedUpdate] = useState(true);
   const router = useRouter();
+  const intervalId = useRef<NodeJS.Timeout>();
+  const [isEditing, setIsEditing] = useState(false);
 
-  const getItems = async () => {
+  const getItems = useCallback(async () => {
+    // if(isEditing) return;
     const url = new URL('/api/items', baseUrl);
     console.log(`url.href:`, url);
     const req = new NextRequest(url);
     console.log(`url:`, url);
     const items = await fetch(new NextRequest(url)).then(res => res.json());
     setItems(items);
-  }
+  }, [isEditing]);
 
   const ListItem = ({ name, completed, id }: { name: string, completed: string, id: string | undefined }) => {
     const [done, setDone] = useState<boolean>((completed === "true") ? true : false);
     const [text, setText] = useState(name);
-    const [oldText, setOldText] = useState(name);
+    // const [oldText, setOldText] = useState(name);
+    const oldTextRef = useRef(name);
+    const textRef = useRef(name);
 
     return (
       <div className='flex-col align-middle justify-center' id={id} >
         <div className='flex justify-between hover:[&_i:text-black] hover:[&_i:font-black]'>
-          <Checkbox   pt={{input:{className:'border-gray-400 border-style-solid border-2 text-[1.25rem]'}, icon:{className:'hover:[stroke-gray-400]'}}} checked={done} onChange={async e => {
+          <Checkbox pt={{ input: { className: 'border-gray-400 border-style-solid border-2 text-[1.25rem]' }, icon: { className: 'hover:[stroke-gray-400]' } }} checked={done} onChange={async e => {
             setDone(e.checked as boolean);
             await fetch(new NextRequest(new URL('/api/item', baseUrl), { method: 'POST', body: JSON.stringify({ name }), headers: { action: 'toggle' } }))
           }} className={`mx-2 my-auto ${done ? ' border-gray-400 decoration-gray-400 bg-gray-400' : 'border-black'}`} />
-          <Inplace unstyled pt={{display:{className:`font-bold text-3xl ${done ? 'line-through text-gray-400' : 'text-black'}`}, content:{className:'text-black text-3xl font-bold'}}}
-          onOpen={() => setOldText(text)} 
-          
-          onBlur={async () => {
-            const url = new URL('/api/item', baseUrl);
-            const req = new NextRequest(url, {method:'POST', body:JSON.stringify({oldName:oldText, newName:text, id}), headers:{action:'update'}});
-            const res = await fetch(req);
-            console.error(`onClose res:`,res);
-          }}>
+          <Inplace unstyled pt={{ display: { className: `font-bold text-3xl ${done ? 'line-through text-gray-400' : 'text-black'}` }, content: { className: 'text-black text-3xl font-bold' } }}
+            onOpen={() => {
+              console.log('onOpen');
+              oldTextRef.current = text
+              setIsEditing(true);
+              clearInterval(intervalId.current);
+            }}
+
+            onBlur={async () => {
+              console.log('onBlur');
+              const url = new URL('/api/item', baseUrl);
+              const req = new NextRequest(url, { method: 'POST', body: JSON.stringify({ oldName: oldTextRef.current, newName: text, id }), headers: { action: 'update' } });
+              const res = await fetch(req);
+              getItems();
+              intervalId.current = setInterval(getItems, 10000);
+              setIsEditing(false);
+            }}>
             <InplaceDisplay >
               {text}
             </InplaceDisplay>
@@ -85,9 +97,17 @@ const List = ({ baseUrl }: { baseUrl: string }) => {
 
   useEffect(() => {
 
-    getItems();
+    console.log(`useEffect`);
 
-    setInterval(getItems, 10000);
+    if (!isEditing) {
+
+      clearInterval(intervalId.current);
+
+      getItems();
+
+      intervalId.current = setInterval(getItems, 10000);
+      console.log(`intervalId.current:`, intervalId.current);
+    }
 
   }, []);
 
@@ -115,4 +135,4 @@ const List = ({ baseUrl }: { baseUrl: string }) => {
 
 export default List
 
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
