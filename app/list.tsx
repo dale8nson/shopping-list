ea6312b93f9bb90a0cb62fd44bb02cdf11439167
dale8nson <daletristanhutchinson@gmail.com>
2024-01-12 11:05:11 +1100

    stylistic adjustments

--- app/list.tsx
+++ app/list.tsx
@@ -42,6 +42,7 @@ const List = ({ baseUrl }: { baseUrl: string }) => {
     const textRef = useRef(name);
 
     const onEdited = async () => {
+      if(text === '' || !!text.match(/^\s+$/)) return;
       const url = new URL('/api/item', baseUrl);
       const req = new NextRequest(url, { method: 'POST', body: JSON.stringify({ oldName: oldTextRef.current, newName: text, id }), headers: { action: 'update' } });
       const res = await fetch(req);
@@ -51,13 +52,13 @@ const List = ({ baseUrl }: { baseUrl: string }) => {
     }
 
     return (
-      <div className='flex-col align-middle justify-center' id={id} >
-        <div className='flex spacing-x-4 hover:[&_i:text-black] hover:[&_i:font-black]'>
+      <div className='flex-col align-baseline justify-center' id={id} >
+        <div className='flex spacing-x-4 align-middle justify-center hover:[&_i:text-black] hover:[&_i:font-black]'>
           <Checkbox pt={{ input: { className: 'border-gray-400 border-style-solid border-2 rounded-md bg-white text-gray-400 stroke-gray-400 text-[1.5rem]' }, root: { className: 'border-none  rounded-md' }, icon: { className: 'hover:[stroke-gray-400] rounded-md' } }} checked={done} onChange={async e => {
             setDone(e.checked as boolean);
             await fetch(new NextRequest(new URL('/api/item', baseUrl), { method: 'POST', body: JSON.stringify({ name, id }), headers: { action: 'toggle' } }))
           }} className={`mx-2 my-auto ${done ? ' border-gray-400 decoration-gray-400 bg-gray-400' : 'border-black'}`} />
-          <Inplace unstyled pt={{ display: { className: `font-bold text-3xl ${done ? 'line-through text-gray-400' : 'text-black'}` }, content: { className: 'text-black text-3xl font-bold' } }}
+          <Inplace unstyled pt={{ root:{className:'align-middle justify-center'}, display: { className: `font-bold xs:text-2xl md:text-3xl align-middle ${done ? 'line-through text-gray-400' : 'text-black'}` }, content: { className: 'text-black xs:text-sm md:text-3xl align-middle font-bold' } }}
             onOpen={() => {
               oldTextRef.current = text
               setIsEditing(true);
@@ -113,7 +114,7 @@ const List = ({ baseUrl }: { baseUrl: string }) => {
       <div className='flex-col lg:my-4 xs:m-0 absolute top-0 left-0 bg-white h-[90vh] w-full overflow-hidden [z-index:0]' >
         <OrderList  className='h-[90vh] w-full lg:left-auto [z-index:2]' {...{ value: items, itemTemplate, header: `Shopping List (${items.length} item${items.length !== 1 ? 's' : ''})`, onChange: e => setItems(e.value) }} pt={{ header: { className: 'relative top-0 left-auto w-full h-12 lg:w-6/12 lg:left-auto mx-auto [z-index:30] !bg-white/30 backdrop-blur-md text-black p-2 font-bold text-3xl border-b-gray-400 border-style-solid border-2 m-0 p-2' }, list: { className: ' max-h-full relative left-0 w-full h-full  [z-index:15]' }, root: { className: 'relative h-[90vh] w-full [z-index:5]' }, container: { className: 'relative h-[90vh] w-full [z-index:10]' }, controls: { className: 'hidden' } }} />
         <div className='flex justify-center space-x-2 fixed lg:w-6/12 bottom-0 left-0 xs:h-[15%] sm:h-[15%] ![z-index:17] bg-black py-4 m-0 w-full lg:left-auto' >
-          <InputText className='text-black text-3xl w-9/12 h-auto ml-0 ' value={itemInputValue} onChange={e => {
+          <InputText className='text-black text-3xl w-10/12 h-auto m-1 ' value={itemInputValue} onChange={e => {
             setItemInputValue(e.target.value);
           }}
             pt={{
@@ -130,7 +131,7 @@ const List = ({ baseUrl }: { baseUrl: string }) => {
               }
             }}
           />
-          <Button severity='secondary' icon='pi pi-plus text-3xl' className='text-3xl w-3/12' pt={{ root: { className: 'bg-black [z-index:25]' }, label:{className:'text-white, [z-index:10]'} }} raised onClick={async () => {
+          <Button severity='secondary' icon='pi pi-plus text-3xl' className='text-3xl w-2/12' pt={{ root: { className: 'bg-black [z-index:25]' }, label:{className:'text-white, [z-index:10]'} }} raised onClick={async () => {
             const uuid = await fetch(new NextRequest(new URL('/api/uuid', baseUrl))).then(res => res.json()).then(json => json.uuid);
             
             await fetch(new NextRequest(new URL(`/api/item`, baseUrl), { method: 'POST', body: JSON.stringify({ name: itemInputValue, id: `${uuid}`}), headers: { action: 'add' }})).then(res => res.json()).then(json => json.uuid);
